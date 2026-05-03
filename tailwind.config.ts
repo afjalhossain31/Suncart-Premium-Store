bfcab090b090f1a65b7fa009885c9d1f61c4d602
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-05-03 23:25:36 +0600

    Vercel deploy problem fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,16 @@
 import type { Config } from "tailwindcss";
-const config: Config = { content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./lib/**/*.{js,ts,jsx,tsx}"], plugins: [require("daisyui")], daisyui: { themes: ["light"] } };
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}"
+  ],
+  plugins: [require("daisyui")],
+  // @ts-ignore - daisyui config
+  daisyui: {
+    themes: ["light"]
+  }
+};
+
 export default config;
